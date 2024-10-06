5692166478b1646540da49f584cc587c3e3d61cb
bruh2288 <cock228cock@gmail.com>
2024-10-06 13:25:16 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from scripts.player import *
 import pygame
 
 flags = pygame.RESIZABLE | pygame.SCALED
@@ -6,11 +7,14 @@ clock = pygame.time.Clock()
 
 FPS = 60
 
-background = pygame.image.load('images\\background.png')
-background = pygame.transform.scale(background, (800, 600))
-bullet = pygame.image.load('images\\bullet.png')
-enemy = pygame.image.load('images\\enemy.png')
-player = pygame.image.load('images\\player.png')
+background_image = pygame.image.load('images\\background.png')
+background = pygame.transform.scale(background_image, (800, 600))
+bullet_image = pygame.image.load('images\\bullet.png')
+enemy_image = pygame.image.load('images\\enemy.png')
+player_image = pygame.image.load('images\\player.png')
+player_image = pygame.transform.scale(player_image, (125,75))
+
+player = Player(400, 550, player_image, 5)
 
 game = True
 
@@ -19,6 +23,9 @@ while game:
         if event.type == pygame.QUIT:
             game = False
 
+    player.update()
+
     window.blit(background, (0,0))
+    player.render(window)
     pygame.display.update()
     clock.tick(FPS)
