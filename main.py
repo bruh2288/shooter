428a4d59ff6fcbabf26de9c04f61a991e113ecaf
bruh2288 <cock228cock@gmail.com>
2024-10-20 13:31:27 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from scripts.player import *
 import pygame
 from scripts.functions import load_image
 from scripts.bullet import Bullet
+from scripts.enemy import Enemy
+from time import time
+from random import randint
 
 flags = pygame.RESIZABLE | pygame.SCALED
 window = pygame.display.set_mode((800, 600), flags)
@@ -16,6 +19,9 @@ player_image = load_image('images\\player.png', (90, 90), None)
 
 player = Player(400, 550, player_image, 5)
 bullets = list()
+enemies = list()
+spawn_delta = 3.5
+timer = time()
 
 game = True
 
@@ -32,10 +38,27 @@ while game:
         bullet.update()
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
+    for enemy in enemies:
+        enemy.update()
+        for bullet in bullets:
+            if enemy.is_colide(bullet):
+                bullets.remove(bullet)
+                enemy.get_damage()
+        if enemy.health <= 0:
+            enemies.remove(enemy)
+    if time() - timer > spawn_delta:
+        timer = time()
+        y = - enemy_image.get_height() // 2
+        x = randint(enemy_image.get_width() / 2, 800 - enemy_image.get_width() // 2)
+        speed = randint(5000, 7000) / 1000
+        health = randint(1, 3)
+        enemies.append(Enemy(x, y, enemy_image, speed, health))
 
     window.blit(background, (0,0))
     player.render(window)
     for bullet in bullets:
         bullet.render(window)
+    for enemy in enemies:
+        enemy.render(window)
     pygame.display.update()
     clock.tick(FPS)
--- scripts/player.py
+++ scripts/player.py
@@ -3,8 +3,12 @@ import pygame
 
 class Player(Sprite):
     def update(self):
+        super().__init__()
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rect.x -= self.speed
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rect.x += self.speed
+
+    def get_damage(self):
+        self.health -= 1
