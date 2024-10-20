0d575911234363c8c93f7364e2ae9898ee21804e
bruh2288 <cock228cock@gmail.com>
2024-10-20 12:05:35 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from scripts.player import *
 import pygame
+from scripts.functions import load_image
+from scripts.bullet import Bullet
 
 flags = pygame.RESIZABLE | pygame.SCALED
 window = pygame.display.set_mode((800, 600), flags)
@@ -7,14 +9,13 @@ clock = pygame.time.Clock()
 
 FPS = 60
 
-background_image = pygame.image.load('images\\background.png')
-background = pygame.transform.scale(background_image, (800, 600))
-bullet_image = pygame.image.load('images\\bullet.png')
-enemy_image = pygame.image.load('images\\enemy.png')
-player_image = pygame.image.load('images\\player.png')
-player_image = pygame.transform.scale(player_image, (125,75))
+background = load_image('images\\background.png', (800, 600), None)
+bullet_image = load_image('images\\bullet.png', (50, 80), (255, 255, 255))
+enemy_image = load_image('images\\enemy.png', (100, 100), (255, 255, 255))
+player_image = load_image('images\\player.png', (90, 90), None)
 
 player = Player(400, 550, player_image, 5)
+bullets = list()
 
 game = True
 
@@ -22,10 +23,19 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bullets.append(Bullet(player.rect.centerx, player.rect.y, bullet_image, 10))
 
     player.update()
+    for bullet in bullets:
+        bullet.update()
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
     window.blit(background, (0,0))
     player.render(window)
+    for bullet in bullets:
+        bullet.render(window)
     pygame.display.update()
     clock.tick(FPS)
